410c5dccdd3ef0122edb52af5e39ca5533043497
nanxiaobei <nanxiaobei@gmail.com>
2022-02-11 03:27:58 +0800

    fix react 18

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { useState, useEffect, useMemo, Dispatch, SetStateAction } from 'react';
 import { unstable_batchedUpdates } from 'react-dom';
 
 type Store = Record<string, any>;
-type Render<T> = { [K in keyof T]: () => T[K] };
-type Actions<T> = { [key in keyof T]: (...args: any[]) => any };
-type Updater<T> = { [key in keyof T]: Set<Dispatch<SetStateAction<T[keyof T]>>> };
+type State<T> = { [K in keyof T]: () => T[K] };
+type Setter<T> = { [key in keyof T]: Set<Dispatch<SetStateAction<T[keyof T]>>> };
 
 const __DEV__ = process.env.NODE_ENV !== 'production';
 const notObj = (val: any) => Object.prototype.toString.call(val) !== '[object Object]';
@@ -12,42 +11,42 @@ const notObj = (val: any) => Object.prototype.toString.call(val) !== '[object Ob
 function resso<T extends Store>(store: T): T {
   if (__DEV__ && notObj(store)) throw new Error('store should be an object');
 
-  const render: Render<T> = {} as Render<T>;
-  const actions: Actions<T> = {} as Actions<T>;
-  const updater: Updater<T> = {} as Updater<T>;
+  const state: State<T> = {} as State<T>;
+  const setter: Setter<T> = {} as Setter<T>;
 
   Object.keys(store).forEach((key: keyof T) => {
-    const initVal = store[key];
-    if (typeof initVal === 'function') {
-      actions[key] = initVal;
-      return;
+    const initValue = store[key];
+    if (typeof initValue !== 'function') {
+      const listeners: Set<Dispatch<SetStateAction<T[keyof T]>>> = new Set();
+      setter[key] = listeners;
+      const Render = () => {
+        const [value, setValue] = useState(initValue);
+        useMemo(() => listeners.add(setValue), []);
+        useEffect(() => () => listeners.delete(setValue) as unknown as void, []);
+        return value;
+      };
+      state[key] = Render;
     }
-    const listeners: Set<Dispatch<SetStateAction<T[keyof T]>>> = new Set();
-    updater[key] = listeners;
-    const Val = () => {
-      const [state, setState] = useState(initVal);
-      useMemo(() => listeners.add(setState), []);
-      useEffect(() => () => listeners.delete(setState) as unknown as void, []);
-      return state;
-    };
-    render[key] = Val;
   });
 
   return new Proxy(store, {
     get(_, key: keyof T) {
-      if (key in actions) return actions[key];
       try {
-        return render[key]();
+        return state[key]();
       } catch (e) {
         return store[key];
       }
     },
     set(_, key: keyof T, val: T[keyof T]) {
       if (val !== store[key]) {
-        unstable_batchedUpdates(() => {
+        const updater = () => {
           store[key] = val;
-          updater[key].forEach((setState) => setState(val));
-        });
+          setter[key].forEach((setValue) => setValue(val));
+        };
+        /* istanbul ignore next */
+        typeof unstable_batchedUpdates === 'function'
+          ? unstable_batchedUpdates(updater)
+          : updater();
       }
       return true;
     },
