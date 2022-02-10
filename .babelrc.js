1e3cf4199eb568f2d47d7ca99db479f2b5bedc3f
nanxiaobei <nanxiaobei@gmail.com>
2022-02-11 03:36:27 +0800

    remove @babel/plugin-transform-runtime

--- .babelrc.js
+++ .babelrc.js
@@ -6,7 +6,6 @@ module.exports = {
   env: {
     test: {
       presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: ['@babel/plugin-transform-runtime'],
     },
   },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,12 @@ import dts from 'rollup-plugin-dts';
 import pkg from './package.json';
 
 const input = 'src/index.ts';
-const deps = Object.keys(pkg.peerDependencies);
-const external = (id) => deps.includes(id) || id.includes('@babel/runtime/');
-const plugins = (useESModules) => [
-  babel({
-    extensions: ['.ts'],
-    plugins: [['@babel/plugin-transform-runtime', { useESModules }]],
-    runtimeHelpers: true,
-  }),
-];
+const peer = Object.keys(pkg.peerDependencies);
+const external = (id) => peer.includes(id);
+const plugins = [babel({ extensions: ['.ts'] })];
 
 export default [
-  {
-    input,
-    output: { file: pkg.main, format: 'cjs', exports: 'auto' },
-    external,
-    plugins: plugins(false),
-  },
-  { input, output: { file: pkg.module, format: 'es' }, external, plugins: plugins(true) },
+  { input, output: { file: pkg.main, format: 'cjs', exports: 'auto' }, external, plugins },
+  { input, output: { file: pkg.module, format: 'es' }, external, plugins },
   { input, output: { file: pkg.types, format: 'es' }, plugins: [dts()] },
 ];
