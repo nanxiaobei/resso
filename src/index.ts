10b47091f041a3deab211fce4977976c896094e4
nanxiaobei <nanxiaobei@gmail.com>
2022-10-14 04:33:09 +0800

    refine actions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
 
 type Callback = () => void;
-type State = Record<string, unknown>;
-type Store<T> = {
+type Method = (...args: unknown[]) => unknown;
+type Store = Record<string, unknown>;
+type State<T> = {
   [K in keyof T]: {
     subscribe: (listener: Callback) => Callback;
     getSnapshot: () => T[K];
@@ -11,57 +12,69 @@ type Store<T> = {
   };
 };
 
+const __DEV__ = process.env.NODE_ENV !== 'production';
+
+let isInMethod = false;
 let run = (fn: Callback) => {
   fn();
 };
 
-const __DEV__ = process.env.NODE_ENV !== 'production';
-
-const resso = <T extends State>(state: T): T => {
-  if (__DEV__ && Object.prototype.toString.call(state) !== '[object Object]') {
+const resso = <T extends Store>(store: T): T => {
+  if (__DEV__ && Object.prototype.toString.call(store) !== '[object Object]') {
     throw new Error('object required');
   }
 
-  const store: Store<T> = {} as Store<T>;
+  const state: State<T> = {} as State<T>;
 
-  Object.keys(state).forEach((key: keyof T) => {
-    if (typeof state[key] === 'function') {
-      return;
+  Object.keys(store).forEach((key: keyof T) => {
+    if (typeof store[key] !== 'function') {
+      const listeners = new Set<Callback>();
+
+      state[key] = {
+        subscribe: (listener) => {
+          listeners.add(listener);
+          return () => listeners.delete(listener);
+        },
+        getSnapshot: () => store[key],
+        setSnapshot: (val) => {
+          if (val !== store[key]) {
+            store[key] = val;
+            run(() => listeners.forEach((listener) => listener()));
+          }
+        },
+        useSnapshot: () => {
+          return useSyncExternalStore(
+            state[key].subscribe,
+            state[key].getSnapshot
+          );
+        },
+      };
     }
-
-    const listeners = new Set<Callback>();
-
-    store[key] = {
-      subscribe: (listener) => {
-        listeners.add(listener);
-        return () => listeners.delete(listener);
-      },
-      getSnapshot: () => state[key],
-      setSnapshot: (val) => {
-        if (val !== state[key]) {
-          state[key] = val;
-          run(() => listeners.forEach((listener) => listener()));
-        }
-      },
-      useSnapshot: () => {
-        return useSyncExternalStore(
-          store[key].subscribe,
-          store[key].getSnapshot
-        );
-      },
-    };
   });
 
-  return new Proxy(state, {
+  return new Proxy(store, {
     get: (_, key: keyof T) => {
+      if (!(key in state)) {
+        return (...args: unknown[]) => {
+          isInMethod = true;
+          const res = (store[key] as Method)(...args);
+          isInMethod = false;
+          return res;
+        };
+      }
+
+      if (isInMethod) {
+        return store[key];
+      }
+
       try {
-        return store[key].useSnapshot();
+        return state[key].useSnapshot();
       } catch (e) {
-        return state[key];
+        return store[key];
       }
     },
     set: (_, key: keyof T, val: T[keyof T]) => {
-      store[key].setSnapshot(val);
+      state[key].setSnapshot(val);
       return true;
     },
   } as ProxyHandler<T>);
